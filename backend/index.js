fa1fac6d1d5641789a47c26edd49f6a0fb7bc246
Godswill Chibuzo <ezechinemere1@gmail.com>
2024-05-10 12:34:05 +0100

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 import Express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import { error } from "console";
+dotenv.config();
 
 const app = Express();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("db connected")
+}).catch((error)=>{
+console.log(error)
+})
+
 
 app.listen(
     3000, ()=>{
